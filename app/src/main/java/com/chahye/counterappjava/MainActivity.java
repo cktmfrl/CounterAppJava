149e5e9ad8d5d7c5dcc50277b9d761f7782f6d8f
origin <sskkccha@gmail.com>
2022-09-26 22:34:51 +0900

    LiveData 적용

--- app/src/main/java/com/chahye/counterappjava/MainActivity.java
+++ app/src/main/java/com/chahye/counterappjava/MainActivity.java
@@ -1,5 +1,6 @@
 package com.chahye.counterappjava;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -9,6 +10,7 @@ import com.chahye.counterappjava.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,17 +19,32 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        MainViewModel viewModel = new ViewModelProvider(this).get(MainViewModel.class);
-        binding.countTextView.setText(String.valueOf(viewModel.count));
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+
+        viewModel.countLiveData.observe(this, count -> {
+            binding.countTextView.setText(String.valueOf(count));
+        });
 
         binding.addButton.setOnClickListener(v -> {
-            viewModel.count++;
-            binding.countTextView.setText(String.valueOf(viewModel.count));
+            viewModel.increaseCount();
         });
 
         binding.subButton.setOnClickListener(v -> {
-            viewModel.count--;
-            binding.countTextView.setText(String.valueOf(viewModel.count));
+            viewModel.decreaseCount();
         });
     }
+
+    // 시스템에 의한 종료 시 UI 상태 유지
+//    @Override
+//    protected void onSaveInstanceState(@NonNull Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//        outState.putInt("count", viewModel.count);
+//    }
+//
+//    @Override
+//    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+//        super.onRestoreInstanceState(savedInstanceState);
+//        viewModel.count = savedInstanceState.getInt("count");
+//    }
+
 }
--- app/src/main/java/com/chahye/counterappjava/MainViewModel.java
+++ app/src/main/java/com/chahye/counterappjava/MainViewModel.java
@@ -1,7 +1,21 @@
 package com.chahye.counterappjava;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
-    int count = 0;
+    private int count = 0;
+
+    MutableLiveData<Integer> countLiveData = new MutableLiveData<>();
+
+    public void increaseCount() {
+        count++;
+        countLiveData.setValue(count);
+    }
+
+    public void decreaseCount() {
+        count--;
+        countLiveData.setValue(count);
+    }
 }
